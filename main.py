29219ddefcb481d80a7bf0c0fa7b12bcca456a6f
coolhandwilliam <coolhandwilliam@gmail.com>
2019-12-06 16:35:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Hunter/Trapper Game')
+money = 100
+guns = 0
+traps = 0
+skins = 0
+def menu():
+  menuinput = input('Type 1 to hunt/trap, 2 to go to store, and 3 to sell hides at the market')
+  if menuinput == "1":
+    
---
+++ usermodule.py
@@ -0,0 +1,9 @@
+print('Hunter/Trapper Game')
+money = 100
+guns = 0
+traps = 0
+skins = 0
+def menu():
+   menuinput = input('Press 1 to go to store, 2 to hunt or trap, and 3 to sell hides.')
+  if menuinput == "1":
+    
